8f666b039d593d7b63d5d1521613a937f0ededd5
ED526 <eddieraydeleon526@gmail.com>
2022-08-03 22:12:20 -0500

    Creation of server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongo = require('./mongo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const route = require('./route/pages');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', route);
+
+mongoose.connect(mongo, {useNewURLParser: true, useUnifiedTopology: true})
+.then(() => {
+    app.listen(3000, ()=> {
+        console.log("Server is running on 3000")
+    })
+})
+
+//username:     FilmRev22
+//pass:         BxgSATCBxgs8X3fM
