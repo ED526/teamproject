3ef38ecaca8397867fc002edb90639d27a3039ef
Benjamin Cardenas <benjaminc5751@gmail.com>
2022-08-07 23:51:57 -0500

    Implemented review UI prototype

---
+++ public/js/review_btn.js
@@ -0,0 +1,13 @@
+const review_btn = document.querySelector("button");
+const post = document.querySelector(".post");
+const widget = document.querySelector(".star-widget");
+const editBtn = document.querySelector(".edit");
+review_btn.onclick = ()=>{
+  widget.style.display = "none";
+  post.style.display = "block";
+  editBtn.onclick = ()=>{
+    widget.style.display = "block";
+    post.style.display = "none";
+  }
+  return false;
+}
