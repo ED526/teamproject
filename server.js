77b3c372017f791eb422c9c14eec8fd105eddbb0
Benjamin Cardenas <benjaminc5751@gmail.com>
2022-08-08 19:57:36 -0500

    initial commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use('/', route);
 
 mongoose.connect(mongo, {useNewURLParser: true, useUnifiedTopology: true})
 .then(() => {
-    app.listen(3000, ()=> {
+    app.listen(process.env.PORT||3000, ()=> {
         console.log("Server is running on 3000")
     })
 })
